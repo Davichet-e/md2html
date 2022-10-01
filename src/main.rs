d2783c648b71756892f6e1eaefd341a1fb49e282
davichete <davidgmorillop@gmail.com>
2022-10-01 16:33:02 +0200

    Added tests, fixed edge case and format html

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use regex::Regex;
+use std::fmt::Write;
+use std::io::{self, Write as IOWrite};
 use std::ops::ControlFlow;
-use std::io::Write;
+use std::{fs, path};
 
 fn parse_line(line: &str) -> String {
     let mut parsed_line = String::new();
@@ -11,10 +13,10 @@ fn parse_line(line: &str) -> String {
     macro_rules! parse {
         ($variable_name:ident, $tag:literal) => {
             if $variable_name {
-                parsed_line.push_str(&format!("</{}>", $tag));
+                write!(parsed_line, "</{}>", $tag).unwrap();
                 $variable_name = false;
             } else {
-                parsed_line.push_str(&format!("<{}>", $tag));
+                write!(parsed_line, "<{}>", $tag).unwrap();
                 $variable_name = true;
             }
         };
@@ -72,7 +74,11 @@ fn parse_text(text: &str) -> String {
 fn get_level_of_heading(heading: &str) -> u8 {
     let value = heading.chars().try_fold(0, |acc, c| {
         if c == '#' {
-            ControlFlow::Continue(acc + 1)
+            if acc > 5 {
+                ControlFlow::Break(0)
+            } else {
+                ControlFlow::Continue(acc + 1)
+            }
         } else {
             ControlFlow::Break(acc)
         }
@@ -83,12 +89,13 @@ fn get_level_of_heading(heading: &str) -> u8 {
     }
 }
 
-fn md2html<P: AsRef<std::path::Path>>(input_md: P, base_html: P, output_html: P) {
-    let parsed_body = parse_text(&std::fs::read_to_string(input_md).unwrap());
-    let base_html = std::fs::read_to_string(base_html).unwrap();
+fn md2html<P: AsRef<path::Path>>(input_md: P, base_html: P, output_html: P) -> io::Result<()> {
+    let parsed_body = parse_text(&fs::read_to_string(input_md)?);
+    let base_html = fs::read_to_string(base_html)?;
     let parsed_html = str::replace(&base_html, "{template}", &parsed_body);
-    let mut output_file = std::fs::File::create(output_html).unwrap();
-    write!(output_file, "{parsed_html}");
+    let mut output_file = fs::File::create(output_html)?;
+    write!(output_file, "{parsed_html}")?;
+    Ok(())
 }
 
 fn main() {
@@ -97,5 +104,98 @@ fn main() {
     println!("{}", get_level_of_heading("### line"));
     println!("{}", get_level_of_heading("sentenc#e"));
     println!("{}", parse_text(include_str!("../in/input.md")));
-    md2html("./in/input.md", "./out/base.html", "./out/parsed.html");
+    md2html("./in/input.md", "./out/base.html", "./out/parsed.html").unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_line() {
+        let test_line = "The `question` ends up being `*_why_*` not *how*";
+
+        let expected =
+            "The <code>question</code> ends up being <code><b><i>why</i></b></code> not <b>how</b>";
+        let actual = parse_line(test_line);
+
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test_get_level_of_heading_returns_3() {
+        let test_line = "### Level #3";
+
+        let expected = 3;
+        let actual = get_level_of_heading(test_line);
+
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test_get_level_of_heading_returns_5() {
+        let test_line = "###### Level ###6";
+
+        let expected = 6;
+        let actual = get_level_of_heading(test_line);
+
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test_get_level_of_heading_returns_0() {
+        let test_line = "Level #3";
+
+        let expected = 0;
+        let actual = get_level_of_heading(test_line);
+
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test_get_level_of_heading_returns_0_when_more_than_6() {
+        let test_line = "####### Overflow";
+
+        let expected = 0;
+        let actual = get_level_of_heading(test_line);
+
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn test_parse_text() {
+        let test_line = r#"
+# Learning AWS
+
+## Day 1
+
+Here is the code I found:
+
+```python
+def h():
+    return "bye"
+```
+
+The question ends up being _why_ not *how*
+
+"#;
+
+        let expected = r#"
+<h1>Learning AWS</h1>
+
+<h2>Day 1</h2>
+
+Here is the code I found:
+
+<pre><code class='language-python'>
+def h():
+    return "bye"
+</code></pre>
+
+The question ends up being <i>why</i> not <b>how</b>
+"#;
+        let actual = parse_text(test_line);
+
+        assert_eq!(expected, actual)
+    }
 }
