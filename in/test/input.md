1ad6b1235ec19c44233336f9f463950bb57394fc
davichete <davidgmorillop@gmail.com>
2022-11-13 18:24:19 +0100

    Add features and tests

--- in/test/input.md
+++ in/test/input.md
@@ -1,10 +1,12 @@
 # My new Blog!
+{date: 2022/10/02}
+{description: Since my first years of college, I always knew I would end up building my own blog from scratch. What I didn't know, is that it would take me so long to start to do it! But finally, I've decided to stop procrastinating it and start building it.}
 
 ## Day 1
 Text p1
 still p2
 
-p2 with link[Rust programming language](https://www.rust-lang.org)
+p2 with link [Rust programming language](https://www.rust-lang.org)
 
 
 He does!
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
+use grouping_by::GroupingBy;
+use lazy_static::lazy_static;
 use regex::Regex;
 use std::fmt::Write;
 use std::io::{self, Write as IOWrite};
 use std::ops::ControlFlow;
+use std::str::Lines;
 use std::{fs, path};
+
+pub struct Article {
+    info: ArticleInfo,
+    content_html: String,
+}
+
+pub struct ArticleInfo {
+    title: String,
+    date: String,
+    description: String,
+}
+
+lazy_static! {
+    static ref CODE_REGEX: Regex = Regex::new(r"```(\w*)").unwrap();
+}
 
 fn parse_line(line: &str) -> String {
     let mut parsed_line = String::new();
@@ -15,33 +33,24 @@ fn parse_line(line: &str) -> String {
     let mut link_text = String::new();
     let mut link_href = String::new();
 
-    macro_rules! parse {
-        ($variable_name:ident, $tag:literal) => {
-            if $variable_name {
-                write!(parsed_line, "</{}>", $tag).unwrap();
-                $variable_name = false;
-            } else {
-                write!(parsed_line, "<{}>", $tag).unwrap();
-                $variable_name = true;
-            }
-        };
+    fn parse(parsed_line: &mut String, variable: &mut bool, tag: &str) {
+        if *variable {
+            write!(parsed_line, "</{}>", tag).unwrap();
+            *variable = false;
+        } else {
+            write!(parsed_line, "<{}>", tag).unwrap();
+            *variable = true;
+        }
     }
-    for c in line.chars() {
-        if c == '_' {
-            parse!(italic, "i");
-        } else if c == '*' {
-            parse!(bold, "b");
-        } else if c == '`' {
-            parse!(inline_code, "code");
-        } else if c == '[' && !is_reading_link_text {
-            is_reading_link_text = true;
-        } else if c == ']' {
-            is_reading_link_text = false;
-        } else if is_reading_link_text {
-            link_text.push(c);
-        } else if c == '(' && !link_text.is_empty() {
-            is_reading_href = true;
-        } else if c == ')' && is_reading_href {
+    line.chars().for_each(|c| match c {
+        '_' => parse(&mut parsed_line, &mut italic, "i"),
+        '*' => parse(&mut parsed_line, &mut bold, "b"),
+        '`' => parse(&mut parsed_line, &mut inline_code, "code"),
+        '[' if !is_reading_link_text => is_reading_link_text = true,
+        ']' => is_reading_link_text = false,
+        _ if is_reading_link_text => link_text.push(c),
+        '(' if !link_text.is_empty() => is_reading_href = true,
+        ')' if is_reading_href => {
             is_reading_href = false;
             write!(
                 parsed_line,
@@ -50,23 +59,20 @@ fn parse_line(line: &str) -> String {
             .unwrap();
             link_href.clear();
             link_text.clear();
-        } else if is_reading_href {
-            link_href.push(c);
-        } else {
-            parsed_line.push(c);
         }
-    }
+        _ if is_reading_href => link_href.push(c),
+        _ => parsed_line.push(c),
+    });
     parsed_line
 }
 
-fn parse_markdown(text: &str) -> String {
+fn parse_markdown(text: &str) -> Article {
     let mut parsed_markdown = String::new();
 
-    let regex = Regex::new(r"```(\w*)").unwrap();
     let mut is_within_code_block = false;
     let mut is_within_paragraph = false;
 
-    let should_end_paragraph = move || {
+    let should_end_paragraph = |is_within_paragraph| {
         if is_within_paragraph {
             "</p>"
         } else {
@@ -74,7 +80,29 @@ fn parse_markdown(text: &str) -> String {
         }
     };
 
-    text.lines().for_each(|line| {
+    let mut text_lines = text.lines();
+
+    let title = text_lines.next().unwrap().strip_prefix("# ").unwrap();
+
+    let date = text_lines
+        .next()
+        .unwrap()
+        .strip_prefix("{date: ")
+        .and_then(|date_str| date_str.strip_suffix('}'))
+        .expect("Expected date format: `{date: YY/MM/dd}`");
+
+    let description = text_lines
+        .next()
+        .unwrap()
+        .strip_prefix("{description: ")
+        .and_then(|date_str| date_str.strip_suffix('}'))
+        .expect("Expected description format: `{description: <text>}`");
+
+    parsed_markdown.push_str("<main>");
+
+    writeln!(parsed_markdown, "<p>{description}</p>").unwrap();
+
+    text_lines.for_each(|line| {
         if is_within_code_block {
             if line == "```" {
                 is_within_code_block = false;
@@ -85,13 +113,13 @@ fn parse_markdown(text: &str) -> String {
         } else {
             let heading_level = get_level_of_heading(line);
             if heading_level == 0 {
-                let code_block_capture = regex.captures(line);
+                let code_block_capture = CODE_REGEX.captures(line);
                 if let Some(capture) = code_block_capture {
                     is_within_code_block = true;
                     write!(
                         parsed_markdown,
                         "{}<pre><code class='language-{}'>",
-                        should_end_paragraph(),
+                        should_end_paragraph(is_within_paragraph),
                         capture.get(1).unwrap().as_str()
                     )
                     .unwrap();
@@ -108,15 +136,23 @@ fn parse_markdown(text: &str) -> String {
                 write!(
                     parsed_markdown,
                     "{}<h{heading_level}>{}</h{heading_level}>",
-                    should_end_paragraph(),
+                    should_end_paragraph(is_within_paragraph),
                     parse_line(&line[(heading_level as usize) + 1..])
                 )
                 .unwrap();
             }
-            parsed_markdown.push('\n')
         }
+        parsed_markdown.push('\n');
     });
-    parsed_markdown
+    parsed_markdown.push_str("</main>");
+    Article {
+        info: ArticleInfo {
+            title: title.to_string(),
+            date: date.to_string(),
+            description: description.to_string(),
+        },
+        content_html: parsed_markdown,
+    }
 }
 
 fn get_level_of_heading(heading: &str) -> u8 {
@@ -137,23 +173,126 @@ fn get_level_of_heading(heading: &str) -> u8 {
     }
 }
 
-fn md2html<P: AsRef<path::Path>>(input_md: P, base_html: P, output_html: P) -> io::Result<()> {
-    let parsed_body = parse_markdown(&fs::read_to_string(input_md)?);
+pub fn md2html<P: AsRef<path::Path>>(
+    input_md: P,
+    base_html: P,
+    output_html: P,
+) -> io::Result<ArticleInfo> {
+    let parsed_article = parse_markdown(&fs::read_to_string(input_md)?);
     let base_html = fs::read_to_string(base_html)?;
 
     let mut output_file = fs::File::create(output_html)?;
-    let parsed_html = base_html.replace("{template}", &parsed_body);
+    let parsed_html = base_html
+        .replace("{template}", &parsed_article.content_html)
+        .replace("{title}", &parsed_article.info.title)
+        .replace("{date}", &parsed_article.info.date);
 
-    write!(output_file, "{parsed_html}")
+    write!(output_file, "{parsed_html}")?;
+
+    Ok(parsed_article.info)
 }
 
 fn main() {
-    md2html("./in/input.md", "./out/base.html", "./out/parsed.html").unwrap();
+    // md2html("./in/test/input.md", "./out/base.html", "./out/parsed.html").unwrap();
+    println!(
+        "{}",
+        parse_markdown(include_str!("../in/test/input.md")).content_html
+    );
+}
+
+fn create_blog_list<P: AsRef<path::Path>>(
+    blog_list_path: P,
+    input_path: P,
+    base_path: P,
+) -> io::Result<()> {
+    let mut file = fs::File::create(blog_list_path)?;
+    let files = fs::read_dir(input_path)?.filter_map(|r| match r {
+        Ok(entry) if entry.file_type().unwrap().is_file() => Some(entry.path()),
+        _ => None,
+    });
+    for path in files {
+        let filename = path.file_stem().unwrap().to_str().unwrap().to_string();
+        let out_dir = base_path.as_ref().join("out/");
+        let article = md2html(
+            path.as_path(),
+            out_dir.join("base.html").as_path(),
+            out_dir.join(filename + ".html").as_path(),
+        )?;
+        writeln!(
+            file,
+            "title: {title}\ndate: {date}\ndescription: {description}\n\n",
+            title = article.title,
+            date = article.date,
+            description = article.description
+        )?;
+    }
+    Ok(())
+}
+
+pub fn read_blog_list(blog_list_path: impl AsRef<path::Path>) -> Vec<ArticleInfo> {
+    let file = fs::read_to_string(blog_list_path).unwrap();
+    file.split("\n\n\n")
+        .map(|line_str| parse_article(line_str.lines()).unwrap())
+        .collect()
+}
+
+fn parse_blog_list(articles: &[&ArticleInfo]) -> String {
+    let mut blog_entries = String::new();
+    for article in articles {
+        writeln!(
+            blog_entries,
+            r#"
+<div class="blog-entry">
+    <h3 class="article-name">{title}</h3>
+    <h4 class="article-date">{date}</h4>
+    <p>
+        {description}
+    </p>
+</div>"#,
+            title = article.title,
+            date = article.date,
+            description = article.description
+        )
+        .unwrap();
+    }
+    blog_entries
+}
+
+fn parse_article(mut raw_article_lines: Lines) -> Option<ArticleInfo> {
+    let title = raw_article_lines.next()?.strip_prefix("title: ")?;
+    let date = raw_article_lines.next()?.strip_prefix("date: ")?;
+    let description = raw_article_lines.next()?.strip_prefix("description: ")?;
+
+    Some(ArticleInfo {
+        title: title.to_string(),
+        date: date.to_string(),
+        description: description.to_string(),
+    })
+}
+
+pub fn build_blog_entry_list(blog_list_path: impl AsRef<path::Path>) -> String {
+    let articles = read_blog_list(blog_list_path);
+    let articles_per_year = articles.iter().grouping_by(|article| &article.date[5..]);
+    let articles_list_html: Vec<String> = articles_per_year
+        .iter()
+        .map(|(year, article_list)| {
+            format!(
+                r#"
+        <h2 class="year">{year}</h2>
+        {blog_entries}
+        "#,
+                blog_entries = parse_blog_list(article_list)
+            )
+        })
+        .collect();
+
+    articles_list_html.join("\n")
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use pretty_assertions::assert_eq;
 
     #[test]
     fn test_parse_line() {
@@ -207,11 +346,53 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_text() {
+    fn test_parse_markdown() {
         let test_line = include_str!("../in/test/input.md");
-        let expected = include_str!("../out/test/expected.html");
+        let expected = include_str!("../out/test/parse_markdown-expected.html");
         let actual = parse_markdown(test_line);
 
-        assert_eq!(expected, actual)
+        assert_eq!(expected, actual.content_html)
+    }
+
+    #[test]
+    fn test_md2html() {
+        let expected = include_str!("../out/test/md2html-expected.html");
+        let output_html_path = "./out/test/md2html-received.html";
+        let _ = md2html("./in/test/input.md", "./out/base.html", output_html_path);
+
+        assert_eq!(expected, &fs::read_to_string(output_html_path).unwrap())
+    }
+
+    #[test]
+    fn test_parse_blog_list() {
+        // Given
+        let date = "2022-08-23".to_string();
+        let description = "This is a description of the article".to_string();
+        let title = "Title of the article".to_string();
+        let article_list = &[&ArticleInfo {
+            date: date.clone(),
+            description: description.clone(),
+            title: title.clone(),
+        }];
+
+        // When
+        let expected_html = parse_blog_list(article_list);
+
+        // Then
+        assert_eq!(
+            expected_html,
+            format!(
+                r#"
+<div class="blog-entry">
+    <h3 class="article-name">{title}</h3>
+    <h4 class="article-date">{date}</h4>
+    <p>
+        {description}
+    </p>
+</div>
+"#,
+            )
+            .to_string()
+        )
     }
 }
